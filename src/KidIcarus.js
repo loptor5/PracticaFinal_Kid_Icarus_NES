23d10a5a914038cbd0d02c81d5ff7da5c26740c5
Demonhero <jogomezt@ucm.es>
2019-05-02 21:27:58 +0200

    Update KidIcarus.js

--- src/KidIcarus.js
+++ src/KidIcarus.js
@@ -4,15 +4,9 @@ var game = function(){
     init: function(p) {
       this._super(p, { sheet: "player", x: 42, y: 2768 });
       this.add('2d, platformerControls');
-      
-      this.on("hit.sprite",function(collision) {
-        if(collision.obj.isA("Tower")) {
-          Q.stageScene("endGame",1, { label: "You Won!" }); 
-          this.destroy();
-        }
-      });
     }
   });
+  
   Q.scene("Level11", function(stage) {
     Q.stageTMX("Level11.tmx", stage);
     const player = stage.insert(new Q.Player());
