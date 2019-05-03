709f96fdf38f6e0e1653f21d0d3a66a7ee9607a0
Demonhero <jogomezt@ucm.es>
2019-05-03 10:39:42 +0200

    .

--- src/data/Level1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="Level1" tilewidth="16" tileheight="16" tilecount="25" columns="5">
- <image source="Level1.png" width="80" height="80"/>
-</tileset>
