973b31078d6e5d1f2024b6e05b58248da655850f
Demonhero <jogomezt@ucm.es>
2019-05-04 18:01:07 +0200

    Update KidIcarus.js

--- src/KidIcarus.js
+++ src/KidIcarus.js
@@ -60,8 +60,8 @@ var game = function(){
     Q.stageTMX("Level101.tmx", stage);
     const player = stage.insert(new Q.Pit());
     stage.add("viewport").follow(player);
-    stage.viewport.offsetX = -400;
-    stage.viewport.offsetY = 640;
+    stage.viewport.offsetX = -1300;
+    stage.viewport.offsetY = 1600;
     
   });
   
