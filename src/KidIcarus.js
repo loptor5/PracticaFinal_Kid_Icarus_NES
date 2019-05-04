d6374c883b78db245c5268c0a152823d0780838a
Demonhero <jogomezt@ucm.es>
2019-05-04 18:08:04 +0200

    Update KidIcarus.js

--- src/KidIcarus.js
+++ src/KidIcarus.js
@@ -60,7 +60,7 @@ var game = function(){
     Q.stageTMX("Level101.tmx", stage);
     const player = stage.insert(new Q.Pit());
     stage.add("viewport").follow(player);
-    stage.viewport.scale= 50;
+    stage.viewport.scale= 10;
     
   });
   
