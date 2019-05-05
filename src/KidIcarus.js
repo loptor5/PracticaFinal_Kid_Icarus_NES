6c454147f645ff751a7313ab63e348c48316efa4
Demonhero <jogomezt@ucm.es>
2019-05-05 21:14:08 +0200

    Update KidIcarus.js

--- src/KidIcarus.js
+++ src/KidIcarus.js
@@ -91,8 +91,8 @@ var game = function(){
     stand_left: {frames:[1], flip: "x", loop:true, rate: 1/5},
     walk_right: {frames: [1,2,3,4], rate: 1/16, flip:false, loop:true, next: "stand_right"},
     walk_left: {frames: [1,2,3,4], rate: 1/16, flip: "x", loop:true, next: "stand_left"},
-    jump_right: {frames: [6,7], flip: false, loop: true, rate: 1/5, next: "stand_right"},
-    jump_left: {frames: [6,7], flip: "x", loop: true, rate: 1/5, next: "stand_left"},
+    jump_right: {frames: [6,7], flip: false, loop: true, rate: 1/5},
+    jump_left: {frames: [6,7], flip: "x", loop: true, rate: 1/5},
     lookUp: {frames: [8,9], rate: 1/5, flip: false, loop: true, next: "stand_right"},
     death: {frames:[0], flip:false, rate:2, loop:false, trigger: "dying"}
   });
