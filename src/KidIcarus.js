dea7230cb079a331473c2758c9cd1529ffd8d7b0
Demonhero <jogomezt@ucm.es>
2019-05-07 20:57:27 +0200

    Update KidIcarus.js

--- src/KidIcarus.js
+++ src/KidIcarus.js
@@ -113,7 +113,7 @@ var game = function(){
     walk_left: {frames: [1,2,3,4], rate: 1/16, flip: "x", loop:true, next: "stand_left"},
     jump_right: {frames: [6,7], flip: false, loop: true, rate: 1/5},
     jump_left: {frames: [6,7], flip: "x", loop: true, rate: 1/5},
-    lookUp: {frames: [8,9], rate: 1/5, flip: false, loop: true},
+    lookUp: {frames: [8,9], rate: 1/5, flip: false, loop: false},
     death: {frames:[0], flip:false, rate:2, loop:false, trigger: "dying"}
   });
   //----------------------------------------------------------------------//
@@ -183,7 +183,7 @@ var game = function(){
         vx:20
       });
 
-      this.add("2d, aiBounce, animation");
+      this.add(" aiBounce, animation");
       this.on("bump.left, bump.right, bump.up, bump.bottom", this, "hit");
       this.on("hit", this, "killed");
     },
