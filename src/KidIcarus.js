4a9fda4088862f7bb1fce2bbc78e595897ab9540
Demonhero <jogomezt@ucm.es>
2019-05-10 10:17:10 +0200

    Update KidIcarus.js

--- src/KidIcarus.js
+++ src/KidIcarus.js
@@ -292,11 +292,11 @@ var game = function(){
         if(this.p.x>=256 || this.p.x<=0){
           this.p.vx= -this.p.vx;
         }*/
-        this.p.time+=2;
+        this.p.time+=0.5;
         var centroX= 256/2;
         var centroY= (this.p.yIni+this.p.yFin)/2;
         var t=this.p.time;
-        scale= 50;
+        scale= 100;
         this.p.x= centroX+scale*Math.cos(t);
         this.p.y= centroY+scale*Math.sin(2*t)/2;
         if((this.p.x>centroX && this.p.y<centroY)||(this.p.x<centroX && this.p.y>centroY)){
