3ea91ac7fc78194bb06999be913c03b692396722
Demonhero <jogomezt@ucm.es>
2019-05-11 11:50:28 +0200

    Update KidIcarus.js

--- src/KidIcarus.js
+++ src/KidIcarus.js
@@ -27,7 +27,7 @@ var game = function(){
         sort: true,
         direction: "right",
         speed: 80,
-        jumpSpeed: -240
+        jumpSpeed: -280
       });
 
       this.add("2d, platformerControls, animation");
