c3ca2525c8251866eb48f84893ceaca352cac568
Demonhero <jogomezt@ucm.es>
2019-05-12 20:08:24 +0200

    Arreglos Directorios

--- PracticaFinal_Kid_Icarus_NES-master/README.md
+++
@@ -1,2 +0,0 @@
-# PracticaFinal_Kid_Icarus_NES
-Kid Icarus de la Nes con quintus
--- PracticaFinal_Kid_Icarus_NES-master/data/Monoculus.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="Monoculus" tilewidth="16" tileheight="16" tilecount="2" columns="2">
- <image source="../images/Monoculus.png" trans="ffffff" width="32" height="16"/>
-</tileset>
