e32a87537fc7967e4a640423ab914d7a0b1279be
Demonhero <jogomezt@ucm.es>
2019-05-12 17:16:56 +0200

    Update KidIcarus.js

--- src/KidIcarus.js
+++ src/KidIcarus.js
@@ -620,7 +620,7 @@ var game = function(){
         heart: 10,
         hit:2,
         vx:10,
-        visible:false
+        visible:true
       });
 
       this.add("2d, aiBounce, animation");
@@ -653,7 +653,7 @@ var game = function(){
 	    if(this.p.live>0){
 	        var pit=Q("Pit");
 	        if(pit.items[0]){
-	        	visible: true;
+	        	this.p.visible: true;
 		        pit= pit.items[0];
 		        if(pit.p.y==this.p.y){
 		          if(pit.p.x-this.p.x>0)
@@ -719,7 +719,7 @@ var game = function(){
     stage.insert(new Q.FunestoM({ x:135, y: 1046, yIni:1049, yFin:1146, time: 0}));
     stage.insert(new Q.Napias({ x:135, y: 1046, yIni:2500, yFin:2600, time: 1}));
     stage.insert(new Q.Netora({ x: 70, y: 2666}));
-    stage.insert(new Q.Netora({ x: 70, y: 2666}));
+    stage.insert(new Q.Fuego({ x: 138, y: 2551}));
     
   });
   
